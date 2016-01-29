2c1e4480a4f0cf623492f69d01b8407ea74909c4
owenllharris <owenllharris@gmail.com>
2016-01-29 20:41:54 +0000

    loads of stuff

---
+++ Assets/Code/FireBall.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBall : MonoBehaviour {
+
+    public float speed = 10f;
+    private Rigidbody rb;
+
+    public GameObject gibs;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = Camera.main.transform.forward * speed;
+        Invoke("Remove", 10f);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Enemy")
+        {
+            gibs.SetActive(true);
+            gibs.transform.parent = null;
+            Remove();
+        }
+    }
+
+
+    void Remove()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Code/SpawnFireball.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFireball : MonoBehaviour {
+
+    public GameObject fireBall;
+    
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Instantiate(fireBall, transform.position + Camera.main.transform.forward, Camera.main.transform.rotation);
+        }
+	}
+}

34151cfba2063610b094032672ef9b0f771f12c3
wavioli <ravioli.croly@gmail.com>
2016-01-30 22:00:50 +0000

    barry gib

--- Assets/Code/Gib.cs
+++ Assets/Code/Gib.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class Gib : MonoBehaviour 
 {
+
+    public AudioClip[] clips;
+    AudioSource a;
 	IEnumerator Start () 
 	{
+        a = GetComponent<AudioSource>();
+        clips = new AudioClip[6];
 		yield return new WaitForSeconds(Random.Range(10f, 20f));
 		Material mat = transform.GetComponent<Renderer> ().material;
 		float elapsed = 0f;
@@ -17,4 +22,8 @@ public class Gib : MonoBehaviour
 		}
 		Destroy (gameObject);
 	}
+
+    void OnCollisionEnter(Collision col) {
+        a.PlayOneShot(clips[Random.Range(0, clips.Length)]);
+    }
 }

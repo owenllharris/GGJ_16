aac3cc7bb7ddb6abcf0d825ef342e8a32a43d2f6
owenllharris <owenllharris@gmail.com>
2016-01-30 14:21:25 +0000

    bunch of stuff

---
+++ Assets/Code/StickToTarget.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickToTarget : MonoBehaviour {
+
+
+    public GameObject target;
+    public Vector3 offset = Vector3.zero;
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.transform.position + offset;
+        transform.rotation = target.transform.rotation;
+	}
+}

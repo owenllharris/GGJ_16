8f2b3e39c1f13820ac9f44c7fde6f1cb3dd3b0d8
owenllharris <owenllharris@gmail.com>
2016-01-30 16:49:33 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Assets/Scenes/VR scene.unity

--- Assets/Code/StickToTarget.cs
+++ Assets/Code/StickToTarget.cs
@@ -5,13 +5,17 @@ public class StickToTarget : MonoBehaviour {
 
 
     public GameObject target;
-    public Vector3 offset = Vector3.zero;
+    public float offsetX;
+    public float offsetY;
+    public float offsetZ;
 
     
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = target.transform.position + offset;
+        transform.position = target.transform.position + (target.transform.right * offsetX) + (target.transform.up * offsetY) + (target.transform.forward * offsetZ);
         transform.rotation = target.transform.rotation;
 	}
+
+
 }
